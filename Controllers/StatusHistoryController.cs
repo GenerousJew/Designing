2ae87257af79c34c383beabad8167d76bef14713
danii <danii@DESKTOP-EPDLMUR>
2023-07-04 23:28:54 +0800

    Дополнил API запросами для четвертой сессии

---
+++ Controllers/StatusHistoryController.cs
@@ -0,0 +1,22 @@
+﻿using NewAPI.Classes;
+using NewAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace NewAPI.Controllers
+{
+    public class StatusHistoryController : ApiController
+    {
+        NewSession1Entities DataBase = new NewSession1Entities();
+
+        public IHttpActionResult GetStatusHistoryByTaskId(int taskId)
+        {
+            var HistoryList = DataBase.StatusHistory.Where(x => x.TaskId == taskId).ToList().ConvertAll(x => new StatusHistoryClass(x));
+
+            return Ok(HistoryList);
+        }
+    }
+}
